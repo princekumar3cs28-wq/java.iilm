085532aa91d58e6b05429501e9d308992bed2fc8
princekumar3cs28-wq <prince.kumar3.cs28@iilm.edu>
2025-08-22 13:11:47 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input first number
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        // Input second number
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Input operator
+        System.out.print("Choose an operation (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+    
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+
+        scanner.close();
+    }
+}
+
